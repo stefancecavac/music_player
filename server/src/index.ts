69d0200f15f69f527bd1f5dff7239d941a828131
stefan <cecavac98@gmail.com>
2025-04-21 17:16:19 +0200

    fixed express pathing

--- server/src/index.ts
+++ server/src/index.ts
@@ -30,7 +30,7 @@ app.use(errroHandler);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../client/dist")));
 
-  app.get("*", (req, res: Response) => {
+  app.get(/(.*)/, (req, res: Response) => {
     res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
   });
 }
